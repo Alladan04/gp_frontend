f0c62d22dde9c54a91054db5e0dc24052283dfa0
Alladan04 <alla.danielyan.2004@mail.ru>
2023-11-26 23:34:28 +0300

    add deploy wfs

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rip_front/',
+  base: '/gp_frontend/',
   server: { port: 3000,
     },
   plugins: [react()],
