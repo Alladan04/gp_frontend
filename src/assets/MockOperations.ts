d8de1d017408086d6f6ec8b67c6c457215f4d571
Alladan04 <alla.danielyan.2004@mail.ru>
2023-12-02 13:28:45 +0300

    mk

--- src/assets/MockOperations.ts
+++ src/assets/MockOperations.ts
@@ -1,15 +1,38 @@
-import defaultImage from './Default.svg';
+//import defaultImage from './Default.svg';
 
 const mockOperations = [
     {
         data:{
         id: 1,
         img_src: "none",
-        name: 'пример операции',
+        name: 'пример операции 1',
         description: 'лололололо',
         status: 'действует',
        },
-       image: defaultImage,
+       image: "https://www.flaticon.com/ru/free-icon/no-photo_4054617",
+       
+    },
+    {
+        data:{
+        id: 2,
+        img_src: "none",
+        name: 'пример операции 2',
+        description: 'лололололо',
+        status: 'действует',
+       },
+       image: "https://www.flaticon.com/ru/free-icon/no-photo_4054617",
+       
+    },
+    {
+        data:{
+        id: 3,
+        img_src: "none",
+        name: 'пример операции 3',
+        description: 'лололололо',
+        status: 'действует',
+       },
+       image: "https://www.flaticon.com/ru/free-icon/no-photo_4054617",
+       
     }
 ]
 
